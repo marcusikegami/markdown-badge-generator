dac1e8052605b172b5e8b1b8a106ad4ab5983fc6
Marcus Ikegami <hikegami.6@gmail.com>
2022-05-16 10:58:59 -0600

    search function fully functional

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,18 +1,34 @@
 import { badgeArr } from "./badgeArr";
+import { useState } from "react";
 
 function Search() {
+    const [filteredBadges, setFilteredBadges] = useState([]);
     function handleSearch(event) {
         const value = event.target.value;
         const badges = badgeArr.filter(el => {
-            el.toLowerCase
-        })
+            return el.name.toLowerCase().includes(value.toLowerCase());
+        });
+        setFilteredBadges(badges);
     }
 
     return (
+    <section>
         <div className="searchWrapper">
             <label for="search">Search Badges</label>
             <input type="search" id="search" onInput={handleSearch} />
         </div>
+        <div className="badge-grid">
+            {filteredBadges.map(element => {
+                return (
+                    <div>
+                        <p>{element.name}</p>
+                        <img src={element.link} alt={element.name} />
+                        <p>{element.badge}</p>
+                    </div>
+                )
+            })}
+        </div>
+    </section>
     )
 }
 export default Search;

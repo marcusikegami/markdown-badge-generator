ccdc643b0631e93e757a282b683bdb2ddadb349b
Marcus Ikegami <hikegami.6@gmail.com>
2022-06-04 12:34:09 -0600

    updated page header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <div className='headerWrapper'>
         <header>
-          <h1>Markdown Badge Generator</h1>
+          <h1>Markdown Badge Repository</h1>
           <div className='repoLink'>
             <a href='https://github.com/Ileriayo/markdown-badges'>Ileriayo Adebiyi's GitHub Repo</a>
           </div>
